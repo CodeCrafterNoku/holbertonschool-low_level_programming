24925557d45f364e70d42d088f8efe79a79f5af5
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-09 14:31:11 +0000

    happy coding

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -3,39 +3,39 @@
 /**
  * is_separator - checks if a character is a separator
  * @c: the character to check
+ *
  * Return: 1 if separator, 0 otherwise
  */
 int is_separator(char c)
 {
-    char separators[] = " \t\n,;.!?\"(){}";
-    int i;
+	char sep[] = " \t\n,;.!?\"(){}";
+	int i;
 
-    for (i = 0; separators[i] != '\0'; i++)
-    {
-        if (c == separators[i])
-            return (1);
-    }
-    return (0);
+	for (i = 0; sep[i] != '\0'; i++)
+	{
+		if (c == sep[i])
+			return (1);
+	}
+	return (0);
 }
 
 /**
  * cap_string - capitalizes all words of a string
  * @str: string to modify
+ *
  * Return: pointer to the modified string
  */
 char *cap_string(char *str)
 {
-    int i;
+	int i;
 
-    if (str[0] >= 'a' && str[0] <= 'z')
-        str[0] = str[0] - 32;
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] = str[0] - 32;
 
-    for (i = 1; str[i] != '\0'; i++)
-    {
-        if (is_separator(str[i - 1]) && (str[i] >= 'a' && str[i] <= 'z'))
-        {
-            str[i] = str[i] - 32;
-        }
-    }
-    return (str);
+	for (i = 1; str[i] != '\0'; i++)
+	{
+		if (is_separator(str[i - 1]) && (str[i] >= 'a' && str[i] <= 'z'))
+			str[i] = str[i] - 32;
+	}
+	return (str);
 }
