d6932b8fa730917248ac7e07b283dc0d8e070997
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-09 10:02:58 +0000

    happy coding

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers from 0 to 9
- *
- * Return: void
- */
+* print_numbers - prints the numbers from 0 to 9
+*
+* Return: void
+*/
 void print_numbers(void)
 {
-    int i; /* Declare the variable outside the loop */
-    
-    for (i = 0; i <= 9; i++)
-    {
-        _putchar(i + '0'); /* Convert integer to character */
-    }
-    _putchar('\n'); /* Print new line */
+int i; /* Declare the variable outside the loop */
+
+for (i = 0; i <= 9; i++)
+{
+_putchar(i + '0'); /* Convert integer to character */
+}
+_putchar('\n'); /* Print new line */
 }
