123e34fd1c089781aecc84c9bd748a15ca2c753d
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-09 13:33:15 +0000

    happy coding

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,34 +1,55 @@
-#include "main.h"
+	#include    "main.h"
+	#include    <limits.h>
 
-/**
- * _atoi - Converts a string to an integer.
- * @s: The string to convert.
- *
- * Return: The integer converted from the string.
- */
-int	_atoi(char *s)
-{
-	int	i = 0, sign = 1, result = 0, found_digit = 0;
-
-	/* Process signs */
-	while (s[i] && ((s[i] < '0' || s[i] > '9')))
+	/**
+	*    _atoi    -    Converts    a    string    to    an    integer.
+	*    @s:    The    string    to    convert.
+	*
+	*    Return:    The    integer    converted    from    the    string.
+	*/
+	int    _atoi(char    *s)
 	{
-		if (s[i] == '-')
-			sign = -sign;
-		else if (s[i] == '+')
-			; /* Do nothing for + */
-		else if (found_digit)
-			break;
-		i++;
+	int    i    =    0,    sign    =    1;
+	unsigned    int    result    =    0;
+	int    found_digit    =    0;
+
+	/*    Process    signs    and    skip    non-digit    characters    */
+	while    (s[i]    &&&&
+    &&
+        (s[i]    <    '0'    ||    s[i]    >    '9'))
+	{
+	if    (s[i]    ==    '-')
+	sign    =    -sign;
+	else    if    (s[i]    ==    '+')
+	{
+	/*    do    nothing    */
+	}
+	i++;
 	}
 
-	/* Process digits */
-	while (s[i] >= '0' && s[i] <= '9')
+	/*    Process    digits    */
+	while    (s[i]    >=    '0'    &&&&
+    &&
+        s[i]    <=    '9')
 	{
-		found_digit = 1;
-		result = result * 10 + (s[i] - '0');
-		i++;
+	found_digit    =    1;
+
+	if    (result    >    INT_MAX    /    10    ||
+	(result    ==    INT_MAX    /    10    &&&&
+    &&
+        (s[i]    -    '0')    >    INT_MAX    %    10))
+	{
+	return ( (sign    ==    1    ?    INT_MAX    :    INT_MIN));
 	}
 
-	return (sign * result);
-}
+	result    =    result    *    10    +    (s[i]    -    '0');
+	i++;
+	}
+
+	if    (!found_digit)
+	return ( (0));
+
+	if    (sign    ==    -1)
+	return ( -(int)result);
+	return ( (int)result);
+	}
