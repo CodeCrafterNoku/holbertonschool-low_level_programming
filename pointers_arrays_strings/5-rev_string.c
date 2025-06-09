004e280ef4badbaded34102eabbc0847ee630147
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-09 12:01:24 +0000

    happy codng

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,23 +1,23 @@
-#include "main.h"
+#include	"main.h"
 
 /**
- * rev_string - Reverses a string
- * @s: The string to reverse
- * Return: void
- */
-void rev_string(char *s)
+*	rev_string	-	Reverses	a	string
+*	@s:	The	string	to	reverse
+*	Return:	void
+*/
+void	rev_string(char	*s)
 {
-    int length = 0;
-    int i;
-    char temp;
+int	length	=	0;
+int	i;
+char	temp;
 
-    while (s[length] != '\0')
-        length++;
+while	(s[length]	!=	'\0')
+length++;
 
-    for (i = 0; i < length / 2; i++)
-    {
-        temp = s[i];
-        s[i] = s[length - i - 1];
-        s[length - i - 1] = temp;
-    }
+for	(i	=	0;	i	<	length	/	2;	i++)
+{
+temp	=	s[i];
+s[i]	=	s[length	-	i	-	1];
+s[length	-	i	-	1]	=	temp;
+}
 }
