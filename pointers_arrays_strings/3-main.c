30973ebb30d172a87b4d98a418180b8d3c8bc9c9
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-09 14:16:13 +0000

    happy coding

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -1,11 +1,18 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
-* main - Entry point
-* Return: Always 0
-*/
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-_puts("Hello, World!");
-return (0);
+	char s1[] = "Hello";
+	char s2[] = "World!";
+
+	printf("%d\n", _strcmp(s1, s2));
+	printf("%d\n", _strcmp(s2, s1));
+	printf("%d\n", _strcmp(s1, s1));
+	return (0);
 }
