f5488d3e47d9cee431f562e53cf7d0244519f797
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-09 13:38:29 +0000

    happy coding

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,55 +1,36 @@
-	#include    "main.h"
-	#include    <limits.h>
+#include "main.h"
+#include <limits.h>
 
-	/**
-	*    _atoi    -    Converts    a    string    to    an    integer.
-	*    @s:    The    string    to    convert.
-	*
-	*    Return:    The    integer    converted    from    the    string.
-	*/
-	int    _atoi(char    *s)
-	{
-	int    i    =    0,    sign    =    1;
-	unsigned    int    result    =    0;
-	int    found_digit    =    0;
+/**
+ * _atoi - Converts a string to an integer
+ * @s: The string to convert
+ *
+ * Return: The converted integer
+ */
+int _atoi(char *s)
+{
+    int sign = 1;
+    int result = 0;
+    int digit_found = 0;
 
-	/*    Process    signs    and    skip    non-digit    characters    */
-	while    (s[i]    &&&&
-    &&
-        (s[i]    <    '0'    ||    s[i]    >    '9'))
-	{
-	if    (s[i]    ==    '-')
-	sign    =    -sign;
-	else    if    (s[i]    ==    '+')
-	{
-	/*    do    nothing    */
-	}
-	i++;
-	}
+    while (*s)
+    {
+        if (*s == '-')
+            sign *= -1;
+        else if (*s >= '0' && *s <= '9')
+        {
+            digit_found = 1;
+            if (result > (INT_MAX / 10) ||
+                (result == (INT_MAX / 10) && (*s - '0') > (INT_MAX % 10)))
+            {
+                return (sign == 1) ? INT_MAX : INT_MIN;
+            }
+            result = result * 10 + (*s - '0');
+        }
+        else if (digit_found)
+            break;
+        s++;
+    }
 
-	/*    Process    digits    */
-	while    (s[i]    >=    '0'    &&&&
-    &&
-        s[i]    <=    '9')
-	{
-	found_digit    =    1;
-
-	if    (result    >    INT_MAX    /    10    ||
-	(result    ==    INT_MAX    /    10    &&&&
-    &&
-        (s[i]    -    '0')    >    INT_MAX    %    10))
-	{
-	return ( (sign    ==    1    ?    INT_MAX    :    INT_MIN));
-	}
-
-	result    =    result    *    10    +    (s[i]    -    '0');
-	i++;
-	}
-
-	if    (!found_digit)
-	return ( (0));
-
-	if    (sign    ==    -1)
-	return ( -(int)result);
-	return ( (int)result);
-	}
+    return (digit_found) ? sign * result : 0;
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -12,3 +12,4 @@ void print_array(int *a, int n);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+int _atoi(char *s);
