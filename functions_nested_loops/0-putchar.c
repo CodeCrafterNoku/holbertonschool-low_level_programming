113d60019223395389c1c63fc892c03315ab97f8
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-09 08:45:48 +0000

    happy coding

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -7,13 +7,13 @@
  */
 int main(void)
 {
-    char c[] = "_putchar\n";
-    int i;
+	char c[] = "_putchar\n";
+	int i;
 
-    for (i = 0; c[i] != '\0'; i++)
-    {
-        _putchar(c[i]);
-    }
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		_putchar(c[i]);
+	}
 
-    return (0);
+	return (0);
 }
