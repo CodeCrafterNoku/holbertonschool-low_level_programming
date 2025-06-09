64da9f5ed821962ba72d18a98203a50ccd79e98f
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-09 09:07:54 +0000

    happy coding

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * _isalpha - Checks for alphabetic character
- * Return: 1 if character is a letter, otherwise return 0
- * @letter: is the variable to be checked for alphabetic character
+ * @letter: The character to check
+ *
+ * Return: 1 if character is a letter, otherwise 0
  */
 int _isalpha(int letter)
 {
-    if ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z'))
-        return (1);
-    else
-        return (0);
+	if ((letter >= 'a' && letter <= 'z') || (letter >= 'A' && letter <= 'Z'))
+		return (1);
+	return (0);
 }
