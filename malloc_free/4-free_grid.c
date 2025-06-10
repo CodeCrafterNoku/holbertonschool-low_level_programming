4bf2204ebdba0caed10bb1f10397643417198698
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-10 10:31:18 +0000

    best coding

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -2,25 +2,25 @@
 #include <stdlib.h>
 
 /**
- * free_grid - Frees a 2 dimensional grid previously created by alloc_grid.
- * @grid: The 2D array to be freed.
- * @height: The height of the grid.
- *
- * Return: Nothing.
- */
+* free_grid - Frees a 2 dimensional grid previously created by alloc_grid.
+* @grid: The 2D array to be freed.
+* @height: The height of the grid.
+*
+* Return: Nothing.
+*/
 void free_grid(int **grid, int height)
 {
-    int i;
+int i;
 
-    if (grid == NULL || height <= 0)
-        return;
+if (grid == NULL || height <= 0)
+return;
 
-    /* Free each row */
-    for (i = 0; i < height; i++)
-    {
-        free(grid[i]);
-    }
+/* Free each row */
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
 
-    /* Free the grid itself */
-    free(grid);
+/* Free the grid itself */
+free(grid);
 }
