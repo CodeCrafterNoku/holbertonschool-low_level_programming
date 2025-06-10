0d42f6fd29b2f699569ea69ece8d3f3a3fa0e12d
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-10 18:00:38 +0000

    happy coding

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,26 +2,25 @@
 #define LISTS_H
 
 #include <stddef.h> /* For size_t */
+#include <stdio.h> /* For dprintf, printf (used in main for context) */
+#include <stdlib.h> /* For EXIT_SUCCESS, EXIT_FAILURE, malloc, free */
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
+ * struct dlistint_s - doubly linked list
+ * @n: integer data
+ * @prev: points to the previous node
  * @next: points to the next node
  *
- * Description: singly linked list node structure
+ * Description: doubly linked list node structure
  * for Holberton project
  */
-typedef struct list_s
+typedef struct dlistint_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
 
-size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
+size_t print_dlistint(const dlistint_t *h);
 
 #endif /* LISTS_H */
