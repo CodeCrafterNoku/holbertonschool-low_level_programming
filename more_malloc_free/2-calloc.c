5910d451c429414217b27ca1787e271704e4aca1
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-10 11:38:55 +0000

    happy coding

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * _calloc - Allocates memory for an array and initializes to zero
@@ -11,18 +10,23 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
-	unsigned int total_size;
+    void *ptr;
+    unsigned int total_size;
+    unsigned int i;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
+    if (nmemb == 0 || size == 0)
+        return (NULL);
 
-	total_size = nmemb * size;
-	ptr = malloc(total_size);
-	if (ptr == NULL)
-		return (NULL);
+    total_size = nmemb * size;
+    ptr = malloc(total_size);
+    if (ptr == NULL)
+        return (NULL);
 
-	memset(ptr, 0, total_size);
+    /* Manually set allocated memory to zero */
+    for (i = 0; i < total_size; i++)
+    {
+        *((char *)ptr + i) = 0;
+    }
 
-	return (ptr);
+    return (ptr);
 }
--- more_malloc_free/2-main.c
+++ more_malloc_free/2-main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <stdlib.h> // Include stdlib.h for malloc and free
 #include "main.h"
 
 int main(void)
 {
     int *array;
     unsigned int n = 5, size = sizeof(int);
+    unsigned int i;
 
     array = _calloc(n, size);
     if (array == NULL)
@@ -13,7 +15,7 @@ int main(void)
         return (1);
     }
 
-    for (unsigned int i = 0; i < n; i++)
+    for (i = 0; i < n; i++)
     {
         printf("Element %d: %d\n", i, array[i]);
     }
