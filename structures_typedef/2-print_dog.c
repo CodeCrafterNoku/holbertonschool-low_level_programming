20e313ae7e055ed1bd13ea73282e4703a817806b
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-10 12:14:05 +0000

    happy coding

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -2,17 +2,17 @@
 #include "dog.h"
 
 /**
- * print_dog - Prints the details of a struct dog
- * @d: Pointer to the struct dog to print
- *
- * Return: Nothing
- */
+* print_dog - Prints the details of a struct dog
+* @d: Pointer to the struct dog to print
+*
+* Return: Nothing
+*/
 void print_dog(struct dog *d)
 {
-    if (d == NULL)
-        return;
+if (d == NULL)
+return;
 
-    printf("Name: %s\n", d->name ? d->name : "(nil)");
-    printf("Age: %.6f\n", d->age);
-    printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+printf("Name: %s\n", d->name ? d->name : "(nil)");
+printf("Age: %.6f\n", d->age);
+printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,35 +2,35 @@
 #define DOG_H
 
 /**
- * struct dog - Dog structure
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
- */
+* struct dog - Dog structure
+* @name: Name of the dog
+* @age: Age of the dog
+* @owner: Owner of the dog
+*/
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 /**
- * init_dog - Initializes a variable of type struct dog
- * @d: Pointer to the struct dog to initialize
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
- *
- * Return: Nothing
- */
+* init_dog - Initializes a variable of type struct dog
+* @d: Pointer to the struct dog to initialize
+* @name: Name of the dog
+* @age: Age of the dog
+* @owner: Owner of the dog
+*
+* Return: Nothing
+*/
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 /**
- * print_dog - Prints the details of a struct dog
- * @d: Pointer to the struct dog to print
- *
- * Return: Nothing
- */
+* print_dog - Prints the details of a struct dog
+* @d: Pointer to the struct dog to print
+*
+* Return: Nothing
+*/
 void print_dog(struct dog *d);
 
 #endif /* DOG_H */
