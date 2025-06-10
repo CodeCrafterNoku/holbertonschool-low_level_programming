b59fcb6070d1e912fbfeb6b7c7d13682a5882dd5
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-10 12:11:04 +0000

    happy coding

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,16 +2,16 @@
 #define DOG_H
 
 /**
- * struct dog - Dog structure
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
- */
+* struct dog - Dog structure
+* @name: Name of the dog
+* @age: Age of the dog
+* @owner: Owner of the dog
+*/
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 /* Function prototypes */
