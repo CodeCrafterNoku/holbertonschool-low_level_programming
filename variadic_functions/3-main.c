cfe69aafe8705879c0cf859d78b3b8f9916a1acf
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-10 16:45:37 +0000

    happy coding

---
+++ variadic_functions/3-main.c
@@ -0,0 +1,9 @@
+#include "variadic_functions.h"
+
+int main(void)
+{
+    print_all("ceis", 'B', 3, "stSchool");
+    print_all("fcsi", 3.14, 'H', 0, "Hello");
+    print_all("", 1, 2, 3);
+    return (0);
+}
---
+++ variadic_functions/3-print_all.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - Prints anything based on format specifiers
+ * @format: List of type specifiers (c, i, f, s)
+ *
+ * Description: Prints arguments based on format string.
+ *              c: char, i: integer, f: float, s: string
+ *              Strings print (nil) if NULL
+ *              Ignores unknown specifiers
+ *              Uses minimal control structures
+ */
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	unsigned int i = 0, j = 0;
+	char *str;
+	const char *sep = "";
+
+	va_start(args, format);
+
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", sep, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s%s", sep, str);
+				break;
+			default:
+				j++;
+				break;
+		}
+		sep = ", ";
+		i++;
+	}
+
+	va_end(args);
+	printf("\n");
+}
