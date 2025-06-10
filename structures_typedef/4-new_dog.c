a7fa210cccb08378fb3f6050ff12de437822243e
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-10 13:06:44 +0000

    happy coding

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -9,12 +9,12 @@
  */
 unsigned int _strlen(char *s)
 {
-    unsigned int len = 0;
+	unsigned int len = 0;
 
-    while (*s++)
-        len++;
+	while (*s++)
+		len++;
 
-    return (len);
+	return (len);
 }
 
 /**
@@ -26,12 +26,12 @@ unsigned int _strlen(char *s)
  */
 char *_strcpy(char *dest, char *src)
 {
-    char *original_dest = dest;
+	char *original_dest = dest;
 
-    while ((*dest++ = *src++))
-        ;
+	while ((*dest++ = *src++))
+		;
 
-    return (original_dest);
+	return (original_dest);
 }
 
 /**
@@ -44,37 +44,37 @@ char *_strcpy(char *dest, char *src)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
-    unsigned int name_len, owner_len;
+	dog_t *new_dog;
+	unsigned int name_len, owner_len;
 
-    if (name == NULL || owner == NULL)
-        return (NULL);
+	if (name == NULL || owner == NULL)
+		return (NULL);
 
-    new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL)
-        return (NULL);
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+		return (NULL);
 
-    name_len = _strlen(name);
-    owner_len = _strlen(owner);
+	name_len = _strlen(name);
+	owner_len = _strlen(owner);
 
-    new_dog->name = malloc(sizeof(char) * (name_len + 1));
-    if (new_dog->name == NULL)
-    {
-        free(new_dog);
-        return (NULL);
-    }
+	new_dog->name = malloc(sizeof(char) * (name_len + 1));
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
 
-    new_dog->owner = malloc(sizeof(char) * (owner_len + 1));
-    if (new_dog->owner == NULL)
-    {
-        free(new_dog->name);
-        free(new_dog);
-        return (NULL);
-    }
+	new_dog->owner = malloc(sizeof(char) * (owner_len + 1));
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
 
-    _strcpy(new_dog->name, name);
-    _strcpy(new_dog->owner, owner);
-    new_dog->age = age;
+	_strcpy(new_dog->name, name);
+	_strcpy(new_dog->owner, owner);
+	new_dog->age = age;
 
-    return (new_dog);
+	return (new_dog);
 }
