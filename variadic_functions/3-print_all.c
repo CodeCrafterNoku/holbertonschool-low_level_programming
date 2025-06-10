35d99718b4df2a1a45c1f7e8ddf4e809a279962f
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-10 16:57:45 +0000

    happy coding

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -7,53 +7,40 @@
  * @format: List of type specifiers (c, i, f, s)
  *
  * Description: Prints arguments based on format string.
- *              c: char, i: integer, f: float, s: string
- *              Strings print (nil) if NULL
- *              Ignores unknown specifiers
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i = 0, j = 0;
-	char *str;
-	const char *sep = "";
-	int printed = 0;
+	unsigned int i = 0;
+	char *str, *sep = "";
 
 	va_start(args, format);
-
 	while (format && format[i])
 	{
 		switch (format[i])
 		{
 			case 'c':
 				printf("%s%c", sep, va_arg(args, int));
-				printed = 1;
 				break;
 			case 'i':
 				printf("%s%d", sep, va_arg(args, int));
-				printed = 1;
 				break;
 			case 'f':
 				printf("%s%f", sep, va_arg(args, double));
-				printed = 1;
 				break;
 			case 's':
 				str = va_arg(args, char *);
 				if (!str)
 					str = "(nil)";
 				printf("%s%s", sep, str);
-				printed = 1;
 				break;
 			default:
 				i++;
-				j++;
 				continue;
 		}
-		if (printed)
-			sep = ", ";
+		sep = ", ";
 		i++;
 	}
-
-	va_end(args);
 	printf("\n");
+	va_end(args);
 }
