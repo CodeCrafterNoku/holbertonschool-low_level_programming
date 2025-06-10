06cb9d0d2f539bc481dd3eefc2896abaa87f49a5
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-10 12:21:05 +0000

    happy coding

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,35 +2,18 @@
 #define DOG_H
 
 /**
-* struct dog - Dog structure
-* @name: Name of the dog
-* @age: Age of the dog
-* @owner: Owner of the dog
-*/
+ * struct dog - Dog structure
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
 struct dog
 {
-char *name;
-float age;
-char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
 
-/**
-* init_dog - Initializes a variable of type struct dog
-* @d: Pointer to the struct dog to initialize
-* @name: Name of the dog
-* @age: Age of the dog
-* @owner: Owner of the dog
-*
-* Return: Nothing
-*/
-void init_dog(struct dog *d, char *name, float age, char *owner);
+typedef struct dog dog_t;
 
-/**
-* print_dog - Prints the details of a struct dog
-* @d: Pointer to the struct dog to print
-*
-* Return: Nothing
-*/
-void print_dog(struct dog *d);
-
-#endif /* DOG_H */
+#endif
