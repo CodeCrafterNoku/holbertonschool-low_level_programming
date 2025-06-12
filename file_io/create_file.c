d7db17a7949434a245b6631ac55f43c6a4d2b1dd
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-12 13:28:14 +0000

    happy coding

--- file_io/create_file.c
+++ file_io/create_file.c
@@ -9,20 +9,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+    int fd;
 
-	if (!filename)
-		return (-1);
+    if (!filename)
+        return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return (-1);
+    fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (fd == -1)
+        return (-1);
 
-	if (text_content)
-		write(fd, text_content, _strlen(text_content));
+    if (text_content)
+        write(fd, text_content, _strlen(text_content));
 
-	close(fd);
-	return (1);
+    close(fd);
+    return (1);
 }
 
 /**
@@ -33,10 +33,10 @@ int create_file(const char *filename, char *text_content)
  */
 int _strlen(char *s)
 {
-	int c = 0;
+    int c = 0;
 
-	while (s[c])
-		c++;
+    while (s[c])
+        c++;
 
-	return (c);
+    return (c);
 }
