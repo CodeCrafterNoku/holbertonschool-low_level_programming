d9d2ded0a33c6db41be7f76cebee66cc62bb3392
CodeCrafterNoku <nokubongangema8@gmail.com>
2025-06-12 13:33:42 +0000

    happy coding

--- file_io/append_text_to_file.c
+++ file_io/append_text_to_file.c
@@ -10,23 +10,23 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+    int fd;
 
-	if (!filename)
-		return (-1);
+    if (!filename)
+        return (-1);
 
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-		return (-1);
+    fd = open(filename, O_WRONLY | O_APPEND);
+    if (fd == -1)
+        return (-1);
 
-	if (text_content)
-	{
-		if (write(fd, text_content, _strlen(text_content)) == -1)
-			return (-1);
-	}
+    if (text_content)
+    {
+        if (write(fd, text_content, _strlen(text_content)) == -1)
+            return (-1);
+    }
 
-	close(fd);
-	return (1);
+    close(fd);
+    return (1);
 }
 
 /**
@@ -37,10 +37,10 @@ int append_text_to_file(const char *filename, char *text_content)
  */
 int _strlen(char *s)
 {
-	int c = 0;
+    int c = 0;
 
-	while (s[c])
-		c++;
+    while (s[c])
+        c++;
 
-	return (c);
+    return (c);
 }
