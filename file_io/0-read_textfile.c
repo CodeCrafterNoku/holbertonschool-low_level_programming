88049a832532e41d7e55e2689a85f23c115ae58f
Nokubonga Ngema <156459137+CodeCrafterNoku@users.noreply.github.com>
2025-06-12 15:18:31 +0200

    Update 0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
-  * read_textfile - ...
-  * @filename: The source file
-  * @letters: Number of letters to reads and prints
-  *
-  * Return: ...
-  */
+ * read_textfile - Reads a text file and prints it to POSIX stdout
+ * @filename: The name of the file to read
+ * @letters: The number of letters to read and print
+ *
+ * Return: The actual number of letters read and printed, or 0 on failure
+ */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, readed;
